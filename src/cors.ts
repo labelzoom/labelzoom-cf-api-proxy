539bc37840e5a77da0d57fe8d9d75a6824f08580
Robert Faust <robert.faust@labelzoom.net>
2024-09-07 21:58:35 -0500

    I used to be a huge Cloudflare fan, but I can't believe they forced me to use tabs rather than spaces. I'm now starting to have doubts. I like to admire my code after I write it, and the tab width is simply too wide in GitHub.

--- src/cors.ts
+++ src/cors.ts
@@ -1,9 +1,9 @@
 const corsHeaders = {
-	"Access-Control-Allow-Origin": "*",
-	"Access-Control-Allow-Methods": "GET,HEAD,POST,PUT,DELETE,OPTIONS",
-	"Access-Control-Max-Age": "86400",
-	"Access-Control-Allow-Credentials": "true",
-	"Access-Control-Expose-Headers": "X-LZ-Request-ID",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,HEAD,POST,PUT,DELETE,OPTIONS",
+    "Access-Control-Max-Age": "86400",
+    "Access-Control-Allow-Credentials": "true",
+    "Access-Control-Expose-Headers": "X-LZ-Request-ID",
 };
 
 /**
@@ -12,33 +12,33 @@ const corsHeaders = {
  * @returns 
  */
 export async function handleOptions(request: Request, env: Env): Promise<Response> {
-	const origin = request.headers.get("Origin") ?? '';
+    const origin = request.headers.get("Origin") ?? '';
 
-	if (!env.LZ_ALLOWED_ORIGINS.includes(origin)) {
-		return new Response(`Origin ${origin} not allowed`, { status: 403 });
-	}
+    if (!env.LZ_ALLOWED_ORIGINS.includes(origin)) {
+        return new Response(`Origin ${origin} not allowed`, { status: 403 });
+    }
 
-	if (
-		request.headers.get("Origin") !== null &&
-		request.headers.get("Access-Control-Request-Method") !== null &&
-		request.headers.get("Access-Control-Request-Headers") !== null
-	) {
-		// Handle CORS preflight requests.
-		return new Response(null, {
-			headers: {
-				...corsHeaders,
-				"Access-Control-Allow-Origin": request.headers.get("Origin") ?? '',
-				"Access-Control-Allow-Headers": request.headers.get("Access-Control-Request-Headers") ?? '',
-			},
-		});
-	} else {
-		// Handle standard OPTIONS request.
-		return new Response(null, {
-			headers: {
-				Allow: "GET,HEAD,POST,PUT,DELETE,OPTIONS",
-			},
-		});
-	}
+    if (
+        request.headers.get("Origin") !== null &&
+        request.headers.get("Access-Control-Request-Method") !== null &&
+        request.headers.get("Access-Control-Request-Headers") !== null
+    ) {
+        // Handle CORS preflight requests.
+        return new Response(null, {
+            headers: {
+                ...corsHeaders,
+                "Access-Control-Allow-Origin": request.headers.get("Origin") ?? '',
+                "Access-Control-Allow-Headers": request.headers.get("Access-Control-Request-Headers") ?? '',
+            },
+        });
+    } else {
+        // Handle standard OPTIONS request.
+        return new Response(null, {
+            headers: {
+                Allow: "GET,HEAD,POST,PUT,DELETE,OPTIONS",
+            },
+        });
+    }
 }
 
 /**
@@ -48,13 +48,13 @@ export async function handleOptions(request: Request, env: Env): Promise<Respons
  * @returns 
  */
 export function responseWithAllowOrigin(response: Response, origin = '*'): Response {
-	// Clone the response so that it's no longer immutable
-	const newResponse = new Response(response.body, response);
+    // Clone the response so that it's no longer immutable
+    const newResponse = new Response(response.body, response);
 
-	// Add cache control headers
-	newResponse.headers.set('Access-Control-Allow-Origin', origin);
-	newResponse.headers.set('Access-Control-Allow-Credentials', 'true');
-	newResponse.headers.set('Access-Control-Expose-Headers', 'X-LZ-Request-ID');
+    // Add cache control headers
+    newResponse.headers.set('Access-Control-Allow-Origin', origin);
+    newResponse.headers.set('Access-Control-Allow-Credentials', 'true');
+    newResponse.headers.set('Access-Control-Expose-Headers', 'X-LZ-Request-ID');
 
-	return newResponse;
+    return newResponse;
 }
--- src/index.ts
+++ src/index.ts
@@ -22,37 +22,37 @@ import { handleOptions, responseWithAllowOrigin } from "./cors";
  * @returns 
  */
 async function handleConversionLog(request: Request<unknown, IncomingRequestCfProperties<unknown>>, env: Env, ctx: ExecutionContext, url: URL): Promise<Response> {
-	try {
-		const requestID = new Date().toISOString().substring(0, 19).replaceAll('-', '/').replaceAll('T', '/').replaceAll(':', '') + '--' + crypto.randomUUID();
-		const loggingEnabled = Math.random() < env.LZ_LOG_SAMPLE_RATE;
-	
-		// Example path: [0]/[1]api/[2]v2/[3]convert/[4]zpl/[5]to/[6]pdf
-		const conversionPathParts = url.pathname.split('/');
-		const sourceFormat = conversionPathParts[4].toLowerCase();
-		const targetFormat = conversionPathParts[6].toLowerCase();
+    try {
+        const requestID = new Date().toISOString().substring(0, 19).replaceAll('-', '/').replaceAll('T', '/').replaceAll(':', '') + '--' + crypto.randomUUID();
+        const loggingEnabled = Math.random() < env.LZ_LOG_SAMPLE_RATE;
+    
+        // Example path: [0]/[1]api/[2]v2/[3]convert/[4]zpl/[5]to/[6]pdf
+        const conversionPathParts = url.pathname.split('/');
+        const sourceFormat = conversionPathParts[4].toLowerCase();
+        const targetFormat = conversionPathParts[6].toLowerCase();
 
-		// TODO: Unwrap Base64 (if applicable) before storing in R2
-	
-		// Clone and log request asynchronously
-		if (loggingEnabled) ctx.waitUntil(Promise.all([
-			env.LZ_R2_BUCKET.put(requestID + `/in.${sourceFormat}`, request.clone().body), // TODO: Set content type
-			env.LZ_R2_BUCKET.put(requestID + '/params.json', url.searchParams.get('params'), {httpMetadata:{contentType:'application/json'}})
-		]));
-	
-		// Generate response
-		const response = await proxyRequestToBackend(request, url, env, requestID);
-	
-		// Clone and log response asynchronously
-		if (loggingEnabled) ctx.waitUntil(
-			env.LZ_R2_BUCKET.put(requestID + `/out.${targetFormat}`, response.clone().body) // TODO: Set content type
-		);
-	
-		// Return response to client
-		return response;
-	} catch (error) {
-		console.error("error logging conversion data", error);
-		return await proxyRequestToBackend(request, url, env);
-	}
+        // TODO: Unwrap Base64 (if applicable) before storing in R2
+    
+        // Clone and log request asynchronously
+        if (loggingEnabled) ctx.waitUntil(Promise.all([
+            env.LZ_R2_BUCKET.put(requestID + `/in.${sourceFormat}`, request.clone().body), // TODO: Set content type
+            env.LZ_R2_BUCKET.put(requestID + '/params.json', url.searchParams.get('params'), {httpMetadata:{contentType:'application/json'}})
+        ]));
+    
+        // Generate response
+        const response = await proxyRequestToBackend(request, url, env, requestID);
+    
+        // Clone and log response asynchronously
+        if (loggingEnabled) ctx.waitUntil(
+            env.LZ_R2_BUCKET.put(requestID + `/out.${targetFormat}`, response.clone().body) // TODO: Set content type
+        );
+    
+        // Return response to client
+        return response;
+    } catch (error) {
+        console.error("error logging conversion data", error);
+        return await proxyRequestToBackend(request, url, env);
+    }
 }
 
 /**
@@ -62,20 +62,20 @@ async function handleConversionLog(request: Request<unknown, IncomingRequestCfPr
  * @returns 
  */
 async function proxyRequestToBackend(request: Request, url: URL, env: Env, requestID = ''): Promise<Response> {
-	const backendUrl = env.LZ_PROD_API_BASE_URL + url.pathname + url.search;
-	const newRequest = new Request(backendUrl, request);
-	let ip = request.headers.get("X-Forwarded-For") ?? '';
-	if (!ip) {
-		ip = request.headers.get("Cf-Connecting-Ip") ?? '';
-	}
-	newRequest.headers.set('X-LZ-IP', ip);
-	newRequest.headers.set('X-LZ-Secret-Key', env.LZ_PROD_API_SECRET_KEY)
-	if (requestID) newRequest.headers.set("X-LZ-Request-ID", requestID);
-	const response = await fetch(newRequest);
+    const backendUrl = env.LZ_PROD_API_BASE_URL + url.pathname + url.search;
+    const newRequest = new Request(backendUrl, request);
+    let ip = request.headers.get("X-Forwarded-For") ?? '';
+    if (!ip) {
+        ip = request.headers.get("Cf-Connecting-Ip") ?? '';
+    }
+    newRequest.headers.set('X-LZ-IP', ip);
+    newRequest.headers.set('X-LZ-Secret-Key', env.LZ_PROD_API_SECRET_KEY)
+    if (requestID) newRequest.headers.set("X-LZ-Request-ID", requestID);
+    const response = await fetch(newRequest);
     
     // Force redirects to be relative because I couldn't get it to work in Spring Boot
     if (response.status === 301 || response.status === 302) {
-		const locationHeader = response.headers.get('Location') ?? '';
+        const locationHeader = response.headers.get('Location') ?? '';
         if (locationHeader.includes('labelzoom.net/')) {
             const url = new URL(locationHeader);
 
@@ -85,44 +85,44 @@ async function proxyRequestToBackend(request: Request, url: URL, env: Env, reque
         }
     }
 
-	return response;
+    return response;
 }
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		const url = new URL(request.url);
+    async fetch(request, env, ctx): Promise<Response> {
+        const url = new URL(request.url);
 
-		// API modifiers
-		if (url.pathname.startsWith('/api/')) {
-			if (request.method === "OPTIONS") {
-				// Handle CORS preflight requests
-				return handleOptions(request, env);
-			}
-			if (url.pathname.startsWith('/api/v2/convert/')) {
-				return responseWithAllowOrigin(
-					await handleConversionLog(request, env, ctx, url),
-					request.headers.get('Origin') ?? '*'
-				);
-			}
+        // API modifiers
+        if (url.pathname.startsWith('/api/')) {
+            if (request.method === "OPTIONS") {
+                // Handle CORS preflight requests
+                return handleOptions(request, env);
+            }
+            if (url.pathname.startsWith('/api/v2/convert/')) {
+                return responseWithAllowOrigin(
+                    await handleConversionLog(request, env, ctx, url),
+                    request.headers.get('Origin') ?? '*'
+                );
+            }
 
-			// Fallthrough behavior, proxy request to Spring Web
-			return responseWithAllowOrigin(
-				await proxyRequestToBackend(request, url, env),
-				request.headers.get('Origin') ?? '*'
-			);
-		} else if (url.pathname === '/api') {
-			// Force trailing slash after /api
-			url.pathname = '/api/';
-			return Response.redirect(url.toString());
-		}
+            // Fallthrough behavior, proxy request to Spring Web
+            return responseWithAllowOrigin(
+                await proxyRequestToBackend(request, url, env),
+                request.headers.get('Origin') ?? '*'
+            );
+        } else if (url.pathname === '/api') {
+            // Force trailing slash after /api
+            url.pathname = '/api/';
+            return Response.redirect(url.toString());
+        }
 
-		// TODO: Eventually we'll wall off access to the rest of the endpoints once everything has been fully migrated
-		// return new Response(`Not found`, { status: 404 });
+        // TODO: Eventually we'll wall off access to the rest of the endpoints once everything has been fully migrated
+        // return new Response(`Not found`, { status: 404 });
 
-		// Fallthrough behavior, proxy request to Spring Web
-		return responseWithAllowOrigin(
-			await proxyRequestToBackend(request, url, env),
-			request.headers.get('Origin') ?? '*'
-		);
-	},
+        // Fallthrough behavior, proxy request to Spring Web
+        return responseWithAllowOrigin(
+            await proxyRequestToBackend(request, url, env),
+            request.headers.get('Origin') ?? '*'
+        );
+    },
 } satisfies ExportedHandler<Env>;
